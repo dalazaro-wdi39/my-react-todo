794c3fac21b77ec4966924183c4337480117aae4
dalazaro <daryl.lazaro@gmail.com>
2017-08-02 14:14:43 -0700

    Completed Sprint 2: added css and font, added TodosContainer and route

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import {Route} from 'react-router'
 import App from '../App'
+import TodosContainer from '../containers/TodosContainer'
 
 export default (
-  <Route path='/' component={App} />
-  // TODO: add a route for /todos
+  <Route path='/' component={App}>
+    {/* NOTE: /todos is nested within the index */}
+    <Route path='/todos' component={TodosContainer} />
+  </Route>
 )
