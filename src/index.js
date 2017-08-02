84420a6e55b18102f362bd426c211aacf2ee9e00
dalazaro <daryl.lazaro@gmail.com>
2017-08-02 13:45:19 -0700

    Completed Sprint 1: added header component and linked to JS files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Router, browserHistory} from 'react-router'
+import routes from './config/routes.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router routes={routes} history={browserHistory}/>,
+  document.getElementById('root')
+);
 registerServiceWorker();
